1e968ad0a866efd9544c9bd316ca8158dc61e562
usman972 <usman083922@gmail.com>
2025-08-25 01:49:34 +0500

    created the ui for topbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
+import { ColorModeContext, useMode } from "./theme";
+import { colors, CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar"
+import DashBoard from "./scenes/dashboard"
+import Team from "./scenes/team"
+import DashBoard from "./scenes/dashboard"
+import DashBoard from "./scenes/dashboard"
+import DashBoard from "./scenes/dashboard"
+import DashBoard from "./scenes/dashboard"
+import DashBoard from "./scenes/dashboard"
+import DashBoard from "./scenes/dashboard"
+import Sidebar from "./scenes/global/Sidebar"
 
 function App() {
-  return (
+  const [theme, colorMode]= useMode();
+  return (<ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
     <div className="app">
-    
+      <main className="content">
+        <Topbar/>
+      </main>
     </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrosweRouter} from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrosweRouter>
+    <BrowserRouter>
     <App />
-    </BrosweRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
