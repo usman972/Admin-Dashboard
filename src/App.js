30b6f72e2b113ef601a683d214a55b44aeeaa990
usman972 <usman083922@gmail.com>
2025-09-07 18:38:21 +0500

    created pie chart and bar chart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
@@ -10,7 +9,7 @@ import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 // import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -31,16 +30,16 @@ function App() {
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-               <Route path="/team" element={<Team />} />
+              <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
-              {/* <Route path="/pie" element={<Pie />} /> */}
+              <Route path="/pie" element={<Pie />} />
               {/* <Route path="/line" element={<Line />} /> */}
               <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
-              {/* <Route path="/geography" element={<Geography />} />  */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
         </div>
